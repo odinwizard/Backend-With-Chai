a09e625f5cc00a2fdd860d300a7a9c9d01302b9b
Anindya Manna <anindyamanna2013@gmail.com>
2024-08-18 20:17:58 +0530

    Add utility codes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv"
+import app from "./app.js"
 import connectDB from "./db/index.js"
 
 
@@ -7,6 +8,21 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+
+    app.on("error", (error) => {
+        console.log("Error", error )
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT} `)
+        
+    })
+})
+.catch( (error) => {
+    console.log("MONGO DB Connection failed!!!", error)
+})
 
 
 
